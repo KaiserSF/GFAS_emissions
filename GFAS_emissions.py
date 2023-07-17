2e30fbe7bb64328cf78442e9c4eda60da0bc0af5
Johannes Kaiser <kaiser@satfire.org>
2023-07-17 18:09:53 +0200

    calculate emissions for both versions

--- GFAS_emissions.py
+++ GFAS_emissions.py
@@ -5,28 +5,7 @@ import numpy as np
 
 # dominant land cover class according to map -> map index to [1:8]!
 LCC = ('SA', 'SAOS', 'AG', 'AGOS', 'TF', 'PEAT', 'EF', 'EFOS')
-
-def read_ef_cvs(fname, write_json=False):
-    df = pd.read_csv(fname)
-    print(f'read {fname} with {df.columns.values}')
-    right_cols = (df.columns.values==['Species','A19_average','gfas_v1p2','perc_change','type'])
-    assert(right_cols.all())
-
-    df = df.rename(columns={'A19_average':'a19','gfas_v1p2':'v1p2'})
-    print(f'new column names: {df.columns.values}')
-
-    print('mapping land cover classes of Andreae 2019 to GFASv1.2: temperate forest is EF, boreal forest is EFOS!')
-    lc_a19_to_v1p2 = {
-        'peat'                  : 'PEAT',
-        'tropical_forest'       : 'TF',
-        'temperate_forest'      : 'EF',
-        'savannah_and_grassland': 'SA',
-        'boreal_forest'         : 'EFOS',
-        'agricultural_residue'  : 'AG' }
-    df.insert(len(df.columns), 'lc', [lc_a19_to_v1p2[x] for x in df['type']])
-
-    print('mapping species names to GRIB short names...')
-    spec_report_to_grib = {
+SPEC_REPORT_TO_GRIB = {
         'Acetaldehyde'  : 'c2h4o'    ,
         'Acetone'       : 'c3h6o'    ,
         'BC_or_EC'      : 'bc'       ,
@@ -43,7 +22,7 @@ def read_ef_cvs(fname, write_json=False):
         'CO2'           : 'co2'      ,
         'DMS'           : 'c2h6s'    ,
         'Ethanol'       : 'c2h5oh'   ,
-        'Formaldehyde'  : 'ch2oh'    ,
+        'Formaldehyde'  : 'ch2o'    ,
         'H2'            : 'h2'       ,
         'Heptanes'      : 'c7h16'    ,
         'Hexanes'       : 'c6h14'    ,
@@ -69,23 +48,42 @@ def read_ef_cvs(fname, write_json=False):
         'Toluene_lump'  : 'toluene'  ,
         'Xylenes'       : 'c8h10'
         }
-    #for k in spec_report_to_grib.keys():
-    #    spec_report_to_grib[k] = spec_report_to_grib[k] + 'fire'
-    df.insert(len(df.columns), 'shortName', [spec_report_to_grib[x] for x in df['Species']])
+
+def read_ef_cvs(fname, write_json=True):
+    df = pd.read_csv(fname)
+    print(f'read {fname} with {df.columns.values}')
+    right_cols = (df.columns.values==['Species','A19_average','gfas_v1p2','perc_change','type'])
+    assert(right_cols.all())
+
+    df = df.rename(columns={'A19_average':'a19','gfas_v1p2':'v1p2'})
+    print(f'new column names: {df.columns.values}')
+
+    print('mapping land cover classes of Andreae 2019 to GFASv1.2: temperate forest is EF, boreal forest is EFOS!')
+    lc_a19_to_v1p2 = {
+        'peat'                  : 'PEAT',
+        'tropical_forest'       : 'TF',
+        'temperate_forest'      : 'EF',
+        'savannah_and_grassland': 'SA',
+        'boreal_forest'         : 'EFOS',
+        'agricultural_residue'  : 'AG' }
+    df.insert(len(df.columns), 'lc', [lc_a19_to_v1p2[x] for x in df['type']])
+
+    print('mapping species names to GRIB short names...')
+    df.insert(len(df.columns), 'shortName', [SPEC_REPORT_TO_GRIB[x] for x in df['Species']])
 
     print('building dictionary all emission factors...')
     ef = {}
     for v in ('v1p2','a19'):
         ef[v] = {}
-        for s in spec_report_to_grib.values():
+        for s in SPEC_REPORT_TO_GRIB.values():
             ef[v][s] = {}
             for l in lc_a19_to_v1p2.values():
                 #print(v,l,s)
                 #print (df.loc[(df['lc']==l) & (df['shortName']==s)])
-                ef[v][s][l] = df.loc[(df['lc']==l) & (df['shortName']==s)].iloc[0][v]
+                ef[v][s][l] = df.loc[(df['lc']==l) & (df['shortName']==s)].iloc[0][v] / 1000
 
         print(f'adding land cover types without distince emission factors, i.e. SAOS, AGOS got {v}...')
-        for s in spec_report_to_grib.values():
+        for s in SPEC_REPORT_TO_GRIB.values():
             ef[v][s]['SAOS'] = ef[v][s]['SA']
             ef[v][s]['AGOS'] = ef[v][s]['AG']
 
@@ -97,14 +95,8 @@ def read_ef_cvs(fname, write_json=False):
         
     return ef
     
-def GFAS_emissions(dm_fname, dlc_fname, ef_fname):
+def GFAS_emissions(dm_fname, dlc_fname, ef_fname, action='emission'):
     
-    # read field of combustion rate, resp. dry matter burnt
-    with open(dm_fname, 'r') as fp:
-        dm = ga.grib_new_from_file(fp)
-    assert(ga.grib_get(dm, 'shortName') == 'crfire')
-    print(ga.grib_get(dm, 'paramId'), ga.grib_get(dm, 'shortName'), ga.grib_get(dm, 'name'))
-
     # read land cover map
     with open(dlc_fname, 'r') as fp:
         dlcmsg = ga.grib_new_from_file(fp)
@@ -114,30 +106,73 @@ def GFAS_emissions(dm_fname, dlc_fname, ef_fname):
     # read species emission factors
     ef = read_ef_cvs(ef_fname)
 
-    # loop over land cover types
-    for il, l in enumerate(LCC):
-
-        # empty output file
-        out_fname = f'Andreae19overGFASv1p2_{l}.grb'
-        with open(out_fname, 'w') as fp:
-            pass
-
-        # loop over species
-        for s in ['nox','co','nh3']:
+    if 'ratio' in action:
+        # RATIOS OF EFS
         
-            # calculate ratios of emission factors
-            emi = ga.grib_clone(dlcmsg)
-            ga.grib_set(emi, 'shortName', s+'fire')
-            #print(f'processing {ga.grib_get(emi,"paramId")} - {ga.grib_get(emi,"shortName")} in {l}...')
-            val = ga.grib_get_values(emi)
-            ratio = ef['a19'][s][l] / ef['v1p2'][s][l]
-            print(f'ratio of {s} in {l}: {ratio}')
-            val = np.where(np.abs(il+1-dlc)<.1, ratio, 1)
-            ga.grib_set_values(emi, val)
+        # loop over land cover types
+        for il, l in enumerate(LCC):
+        
+            # empty output file
+            out_fname = f'Andreae2019_over_GFASv1p2_{l}.grb'
+            with open(out_fname, 'w') as fp:
+                pass
+        
+            # loop over species
+            for s in ['nox','co','nh3']:
             
-            # write species emission fluxes
-            with open(out_fname, 'ab') as fp:
-                ga.grib_write(emi, fp)
+                # calculate ratios of emission factors
+                emi = ga.grib_clone(dlcmsg)
+                ga.grib_set(emi, 'shortName', s+'fire')
+                #print(f'processing {ga.grib_get(emi,"paramId")} - {ga.grib_get(emi,"shortName")} in {l}...')
+                val = ga.grib_get_values(emi)
+                ratio = ef['a19'][s][l] / ef['v1p2'][s][l]
+                print(f'ratio of {s} in {l}: {ratio}')
+                val = np.where(np.abs(il+1-dlc)<.1, ratio, 1)
+                ga.grib_set_values(emi, val)
+                
+                # write species emission fluxes
+                with open(out_fname, 'ab') as fp:
+                    ga.grib_write(emi, fp)
+
+    if 'emission' in action:
+        # EMISSIONS
+        
+        # read field of combustion rate, resp. dry matter burnt
+        with open(dm_fname, 'r') as fp:
+            dmmsg = ga.grib_new_from_file(fp)
+        assert(ga.grib_get(dmmsg, 'shortName') == 'crfire')
+        print(ga.grib_get(dmmsg, 'paramId'), ga.grib_get(dmmsg, 'shortName'), ga.grib_get(dmmsg, 'name'))
+        dm = ga.grib_get_values(dmmsg)
+        
+        # loop over emission factor set versions
+        for v in ('a19','v1p2'):
+        
+            # empty output file
+            out_fname = f'emissions_GFAS_{v}.grb'
+            with open(out_fname, 'w') as fp:
+                pass
+        
+            # loop over species
+            for s in SPEC_REPORT_TO_GRIB.values():
+                print(f'processing {v},{s}')
+                
+                # calculate ratios of emission factors
+                emi = ga.grib_clone(dlcmsg)
+                ga.grib_set(emi, 'shortName', s+'fire')
+                val = ga.grib_get_values(emi)
+                #print(val)
+                val[:] = 0
+                #print(val)
+                  
+                # loop over land cover types
+                for il, l in enumerate(LCC):
+                    val += np.where(np.abs(il+1-dlc)<.1, dm*ef[v][s][l], 0)
+   
+                ga.grib_set_values(emi, val)
+               
+                # write species emission fluxes
+                with open(out_fname, 'ab') as fp:
+                    ga.grib_write(emi, fp)
 
 if __name__ == '__main__':
-    GFAS_emissions('fields.grb', 'dat/dlc.grb', 'dat/Table2_GFAS_vs_A19_EF_summary_longformat.csv')
+    GFAS_emissions('ADSfields.grib', 'dat/dlc.grb', 'dat/Table2_GFAS_vs_A19_EF_summary_longformat.csv', 'ratio+emissions')
